cfcff47b63281224217bbc564bcf17966dad79ed
Nour Hussin <n0ur0eldiin@gmail.com>
2023-09-05 16:24:15 +0300

    Add assignments types module

--- Basics/Assignments.v
+++ Basics/Assignments.v
@@ -4,11 +4,51 @@
 */
 
 module Assignments(
-
+    input a,b,c,
+    output reg x1,x2,x3,x4,
     output[31:0] number1,number2,number3,number4,
     output signed[31:0] number5
 );
 
+    // Blocking assignments
+    always @(a, b, c)
+    begin
+        x1 = a;
+        x1 = x1 ^ b; // it is (a ^ b) because x1 here is equal to a
+        x1 = x1 | c; // it is (a^b)|c because x1 here is equal to a^b
+    end
+    
+    // Nonblocking assignments
+    always @(a, b, c)
+    begin
+        x2 <= a;
+        x2 <= x2 ^ b;
+        x2 <= x2 | c; /* Only the last one will be considered 
+        x2 is feedback or gate with c*/
+    end    
+
+    // Changing the order of blocking assignments
+    always @(a, b, c)
+    begin
+        x3 = a;
+        x3 = x3| c; // it is (a | c) because here x3 is equal to a
+        x3 = x3 ^ b;// it is (a|c)^b because here x3 is equal to (a|c)
+    end
+
+    // Changing the order of nonblocking assignments
+    always @(a, b, c)
+    begin
+        x4 <= a;
+        x4 <= x4 | c;
+        x4 <= x4 ^ b; /* Only the last one will be considered 
+        x4 is feedback xor gate with b*/
+    end  
+
+
+    /* Important Notes:
+        - In a combinational always block, use blocking assignments 
+        - In a clocked always block, use non-blocking assignments
+    */
 
     //! Writing Numbers: (You need not revise number systems and 2's complement)
     /* (-) (Size) ' (s) (Base) (Value)
